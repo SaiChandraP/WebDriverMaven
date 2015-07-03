987c25b2189cbe848d3aa75a34fde61e1a811165
saichandrap <padmanabuni110391@gmail.com>
2015-07-03 15:37:59 +0100

    my first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup(){
+	
+	driver= new FirefoxDriver();
+	
+	}
+
+@Test
+public void doLogin(){
+	
+	driver.get("http://gmail.com");
+	driver.findElement(By.id("Email")).sendKeys("eurodrugtrial@gmail.com");
+	
+
+}
+
+@AfterSuite
+
+public void teardown(){
+	
+	driver.quit();
+}
+}
